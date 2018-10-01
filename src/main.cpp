0636abffb10f2d7edbedde59484734188f227f50
Jules Verne <jules00verne@gmail.com>
2018-10-01 12:34:45 +0200

    Update nStakeMinAge

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ bool fVerifyingBlocks = false;
 unsigned int nCoinCacheSize = 5000;
 bool fAlerts = DEFAULT_ALERTS;
 
-unsigned int nStakeMinAge = 3 * 60 * 60;
+unsigned int nStakeMinAge = 3 * 10 * 1;
 int64_t nReserveBalance = 0;
 
 /** Fees smaller than this (in duffs) are considered zero fee (for relaying and mining)
