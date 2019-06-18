05664092f60d359834ec57b74d6b1792757d0b00
JSKitty <loopylew113@protonmail.com>
2019-06-18 19:15:08 +0100

    [Misc] Fix tiny timedata memory leak
    The timedata samples are currently allowed to be unlimited in quantity, which, after many thousands of node connections over a wallet's lifetime, could mount up from a couple extra KBs of RAM, to 1 or 2 MB of RAM after tens of thousands, while this is an absolutely tiny issue for the next 5 - 10 years, it's still worth futureproofing.

--- src/timedata.cpp
+++ src/timedata.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2014 The Bitcoin developers
+// Copyright (c) 2018-2019 The ZENZO developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -40,6 +41,8 @@ static int64_t abs64(int64_t n)
     return (n >= 0 ? n : -n);
 }
 
+#define ZENZO_TIMEDATA_MAX_SAMPLES 200
+
 void AddTimeData(const CNetAddr& ip, int64_t nTime)
 {
     int64_t nOffsetSample = nTime - GetTime();
@@ -47,11 +50,13 @@ void AddTimeData(const CNetAddr& ip, int64_t nTime)
     LOCK(cs_nTimeOffset);
     // Ignore duplicates
     static set<CNetAddr> setKnown;
+    if (setKnown.size() == ZENZO_TIMEDATA_MAX_SAMPLES)
+        return;
     if (!setKnown.insert(ip).second)
         return;
 
     // Add data
-    static CMedianFilter<int64_t> vTimeOffsets(200, 0);
+    static CMedianFilter<int64_t> vTimeOffsets(ZENZO_TIMEDATA_MAX_SAMPLES, 0);
     vTimeOffsets.input(nOffsetSample);
     LogPrintf("Added time data, samples %d, offset %+d (%+d minutes)\n", vTimeOffsets.size(), nOffsetSample, nOffsetSample / 60);
 
@@ -91,7 +96,7 @@ void AddTimeData(const CNetAddr& ip, int64_t nTime)
 
                 if (!fMatch) {
                     fDone = true;
-                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong Zenzo Core will not work properly.");
+                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong ZENZO Core will not work properly.");
                     strMiscWarning = strMessage;
                     LogPrintf("*** %s\n", strMessage);
                     uiInterface.ThreadSafeMessageBox(strMessage, "", CClientUIInterface::MSG_WARNING);
