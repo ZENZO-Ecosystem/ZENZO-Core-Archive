c800ce4c512936b7c5b1088fbd7cc6f530a6760c
Kolby Moroz <mrmetech@gmail.com>
2019-08-31 07:47:34 -0600

    Update Accumulator.cpp

--- src/libzerocoin/Accumulator.cpp
+++ src/libzerocoin/Accumulator.cpp
@@ -27,7 +27,7 @@ Accumulator::Accumulator(const AccumulatorAndProofParams* p, const CoinDenominat
 	this->value = this->params->accumulatorBase;
 }
 
-Accumulator::Accumulator(const ZerocoinParams* p, const CoinDenomination d, const BigNum bnValue) {
+Accumulator::Accumulator(const ZerocoinParams* p, const CoinDenomination d, const Bignum bnValue) {
 	this->params = &(p->accumulatorParams);
     denomination = d;
 
